614a364f812eb534e14f04a4806a0c12e5add03e
yoshifujiT <t.yoshifuji@me.com>
2019-06-16 02:09:09 +0900

    prefetch 追加

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const PostLink = props => {
   const {url_slug, title} = props.fields
   return (
-    <Link as={`/post/${url_slug}`} href={`/postitem?url_slug=${url_slug}`}>
+    <Link prefetch as={`/post/${url_slug}`} href={`/postitem?url_slug=${url_slug}`}>
       <a>{title}</a>
     </Link>
   )
