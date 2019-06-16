92715366fb517a68a1ebb6a91e0e3d0e83d156f0
hiracky16 <h.piiice16@gmail.com>
2019-06-16 12:34:35 +0900

    リファクタリング #4

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react'
 import ReactMapGL from 'react-map-gl';
-
-const CENTER_POSITION = {
-    lat: 139.8132971,
-    lng: 35.710332
-};
-
-const BASE_URL = 'https://api.mapbox.com/styles/v1/mapbox/streets-v9';
+// const CENTER_POSITION = new mapboxgl.LngLat(139.8132971, 35.710332).wrap();
 
 class Map extends Component {
   state = {
       viewport: {
           width: '100vw',
           height: '100vh',
-          latitude: 37.7577,
-          longitude: -122.4376,
+          latitude: 35.710332,
+          longitude: 139.8132971,
           zoom: 13
       }
   };
@@ -32,55 +26,3 @@ class Map extends Component {
 }
 
 export default Map;
-
-
-/**
-class Map extends Component {
-  componentDidMount = async () => {
-    const style = await fetch(`${BASE_URL}?access_token=${mapboxgl.accessToken}`).then(res => res.json())
-
-    this.map = new mapboxgl.Map({
-      container: this.container,
-      style,
-      center: [CENTER_POSITION.lng, CENTER_POSITION.lat],
-      zoom: 12
-    });
-
-    await this.map.on('load', () => {
-      this.map.addLayer({
-        id: "locations",
-        type: "symbol",
-        // Add a GeoJSON source containing place coordinates and information.
-        source: {
-          type: "geojson",
-          data: './test.json'
-        },
-        layout: {
-          "icon-image": "star-15",
-          "icon-allow-overlap": true,
-          "icon-size": 1.00
-        }
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    this.map.remove();
-  }
-
-  render() {
-    return (
-      <div className={'map'} ref={e => (this.container = e)}>
-      <style jsx>{`
-        .map {
-          width: '100vw';
-          height: '100vh';
-        }
-      `}</style>
-      </div>
-    );
-  }
-}
-
-export default Map
-*/
